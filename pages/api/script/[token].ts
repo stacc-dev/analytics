b0b060e7ee531f562b202f143929b426e643a5ab
Felix <felix.mattick@gmail.com>
2020-07-18 16:48:50 -0400

    Fix analytics domain x2

--- pages/api/script/[token].ts
+++ pages/api/script/[token].ts
@@ -7,5 +7,5 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (!token) return res.status(400).send('No token specified')
 
   res.setHeader('Content-Type', 'application/javascript')
-  return res.status(200).send(`(async()=>{let r=await fetch('http://localhost:3000/api/track',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({referrer:document.referrer,token:'${token}',href:location.href})});if(!r.ok)throw new Error(await r.text())})()`)
+  return res.status(200).send(`(async()=>{let r=await fetch('hhttps://analytics.stacc.cc/api/track',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({referrer:document.referrer,token:'${token}',href:location.href})});if(!r.ok)throw new Error(await r.text())})()`)
 }
