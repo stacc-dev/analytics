0f9260a100cc6fa6115f90ee4f7f8ab6067db248
Andrew Kachnic <ajkachnic@protonmail.com>
2020-07-18 07:47:03 -0400

    Fixed errros with Router

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,14 +1,21 @@
 import { useAuthState } from 'react-firebase-hooks/auth'
 import firebase from 'lib/client/firebase'
 import { useRequireUser, useAuthedData } from 'lib/client/hooks'
-import Router from 'next/router'
+import { useRouter } from 'next/router'
 import { Project } from 'lib/isomorphic/types'
 
 export default () => {
+  const router = useRouter()
   const [ user, loading ] = useAuthState(firebase.auth())
   useRequireUser(user, loading)
-  const project = useAuthedData<Project>(`/api/projects/get/${Router.query.id}`, user)
+  const project = useAuthedData<Project>(`/api/projects/get/${router.query.id}`, user)
   if (!project.data || !user) return 'Loading...'
   if (project.error) return `Error loading project: ${project.error.message}`
-  return `hey look a project owo: ${project.data.name} ehehehe`
+  return (
+    <main>
+      <h1>Project: {project.data.name}</h1>
+      <h2>Setup</h2>
+      <p>To setup our analytics tracking, you need to load our client script. Here are some examples of how to do so</p>
+    </main>
+  )
 }
